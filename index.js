1238ee853ff1e2a51b76f3b138f8a290a13f5ae8
StormThorn91 <caitvsistoso@gmail.com>
2023-07-07 00:00:29 +0800

    Added offline capability that saves last forex amount when api call fails

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 const APILink = 'https://www.freeforexapi.com/api/live?pairs=USDPHP';
 const handlingPercent = 0.15;
 
-window.onload = function() {
+window.onload = function () {
     var form = document.querySelector("form");
     form.onsubmit = calculation.bind(form);
 }
 
 const conversion = async (val) => {
+    let returnValue;
     document.getElementById("submit").disabled = true;
-    let apiResponse = await fetch('https://cors-anywhere.herokuapp.com/' + APILink).then((response) => 
+    let apiResponse = await fetch('https://cors-anywhere.herokuapp.com/' + APILink).then((response) =>
         response.json())
         .catch((err) => {
-        console.log('Something went wrong :( ' + err)
-    });
+            console.log('Something went wrong :( ' + err)
+        });
 
-    let returnValue = await val * apiResponse.rates.USDPHP.rate;
-    console.log(apiResponse.rates.USDPHP.rate);
+    if (apiResponse) {
+        localStorage.setItem('usdPhpConversion', apiResponse.rates?.USDPHP?.rate)
+        returnValue = await val * apiResponse.rates?.USDPHP?.rate;
+        console.log(apiResponse.rates.USDPHP.rate);
+        document.getElementById("conversion-amount").innerHTML = 'Php 1.00 = $ ' + Number(apiResponse.rates.USDPHP.rate).toFixed(2);
+    }
+
+    else {
+        let forexValue = localStorage.getItem('usdPhpConversion');
+        returnValue = val * forexValue;
+        console.log(forexValue);
+        document.getElementById("conversion-amount").innerHTML = 'Php 1.00 = $ ' + Number(forexValue).toFixed(2);
+    }
     console.log(returnValue);
-    document.getElementById("conversion-amount").innerHTML = 'Php 1.00 = $ ' + Number(apiResponse.rates.USDPHP.rate).toFixed(2);
+
+
 
     return returnValue
 }
@@ -35,7 +48,7 @@ const calculation = async (e) => {
     var handlingAmount = taxedAmount * handlingPercent;
     var withHandlingAmount = taxedAmount + handlingAmount;
     var totalAmount = Number(await conversion(withHandlingAmount)).toFixed(2);
-    
+
     document.getElementById("amount").innerHTML = 'Php ' + totalAmount;
     document.getElementById("submit").disabled = false;
 }
