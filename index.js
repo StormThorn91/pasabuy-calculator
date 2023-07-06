228786eef161f872968a473f84776b0e737cf688
Caitlin Sistoso <caitvsistoso@gmail.com>
2023-07-06 18:15:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const APILink = 'https://www.freeforexapi.com/api/live?pairs=USDPHP';
 const handlingPercent = 0.15;
 
-const conversion = (val) => {
-    fetch(APILink).then((response) => {
-        console.log(response)
-    }).catch((err) => {
+const conversion = async (val) => {
+    document.getElementById("submit").disabled = true;
+    let apiResponse = await fetch('https://cors-anywhere.herokuapp.com/' + APILink).then((response) => 
+        response.json())
+        .catch((err) => {
         console.log('Something went wrong :( ' + err)
     });
+
+    let returnValue = await val * apiResponse.rates.USDPHP.rate;
+    document.getElementById("conversion-amount").innerHTML = 'Php 1.00 = $ ' + Number(apiResponse.rates.USDPHP.rate).toFixed(2);
+
+    return returnValue
 }
 
-const calculation = () => {
+const calculation = async () => {
     const realAmount = document.getElementById('real-amount').value;
     const discountPercent = document.getElementById('discount-percent').value / 100;
     const taxPercent = document.getElementById('tax-percent').value / 100;
@@ -17,9 +23,12 @@ const calculation = () => {
     var discountAmount = realAmount * discountPercent;
     var discountedAmount = realAmount - discountAmount;
     var taxAmount = discountedAmount * taxPercent;
-    var taxedAmount = discountedAmount - taxAmount;
+    var taxedAmount = discountedAmount + taxAmount;
     var handlingAmount = taxedAmount * handlingPercent;
-    var withHandlingAmount = taxedAmount - handlingAmount;
+    var withHandlingAmount = taxedAmount + handlingAmount;
+    var totalAmount = Number(await conversion(withHandlingAmount)).toFixed(2);
+    var conversionAmount
 
-    conversion(withHandlingAmount);
+    document.getElementById("amount").innerHTML = 'Php ' + totalAmount;
+    document.getElementById("submit").disabled = false;
 }
